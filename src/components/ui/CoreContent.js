4e9b8bb8987782daa5e61007a39a4ec0dfe6263f
sastrakhan <sastrakhan@gmail.com>
2018-06-17 11:10:19 -0400

    Add Browser-Router

--- src/components/ui/CoreContent.js
+++ src/components/ui/CoreContent.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Entries from "./Entries";
 import IdeaForm from "./IdeaForm";
+import {Route} from 'react-router-dom'
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
 
 const CoreContent = (props) => {
+
   return (
-    <div>
-      <IdeaForm addEntry={props.addEntry}/>
-      <Entries entries={props.entries} />
-    </div>
+    <main>
+     <switch>
+         <Route path='/' render={(p) => (<Entries {...p} entries={props.entries} />)}/>
+         <Route path='/ideaForm' component={IdeaForm}/>
+         <Route path='/about' component={About} />
+      </switch>  
+    </main>
   );
 }
 
